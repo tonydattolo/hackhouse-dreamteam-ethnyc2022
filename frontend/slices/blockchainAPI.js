7c3f304150ce4928fc5b720e94df9d407617ca91
Tony Dattolo <anthonydattolo@gmail.com>
2022-06-24 23:39:58 -0400

    project init

---
+++ frontend/slices/blockchainAPI.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const blockchainApi = createApi({
+  name: "blockchainApi",
+  baseQuery: fetchBaseQuery({
+    url: "https://api.blockchain.info/",
+    prepareHeaders: (headers, { getState }) => {
+      headers.set("Accept: application/json");
+      headers.set("Content-Type", "application/json");
+      return headers
+    },
+  }),
+  // tagTypes: ["blockchain"],
+  endpoints: (builders) => ({
+    // insert api calls here as needed
+  }),
+});
+    
